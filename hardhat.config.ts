cad8fe07c34959561ae6fe572d241b83bc425537
Gabriel Cardona <gabriel@avalabs.org>
2021-05-21 17:02:01 -0700

    Syntax.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,20 +5,18 @@ import "@nomiclabs/hardhat-waffle"
 
 task("accounts", "Prints the list of accounts", async (args, hre): Promise<void> => {
   const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
+  accounts.forEach((account: SignerWithAddress): void => {
     console.log(account.address)
-  }
-});
+  })
+})
 
 task("balances", "Prints the list of AVAX account balances", async (args, hre): Promise<void> => {
   const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
+  accounts.forEach(async (account: SignerWithAddress): Promise<void> => {
     const balance: BigNumber = await hre.ethers.provider.getBalance(account.address)
     console.log(`${account.address} has balance ${balance.toString()}`)
-  }
-});
+  })
+})
 
 export default {
   solidity: {
