4ee5cd37374c12ab806f288d8483a7ea85dbc5b9
Gabriel Cardona <gabriel@avalabs.org>
2021-05-24 07:44:10 -0700

    Formatting.

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -8,7 +8,7 @@ contract Storage {
     number = num;
   }
 
-  function retrieve() public view returns (uint256){
+  function retrieve() public view returns (uint256) {
     return number;
   }
 }
--- scripts/storage.ts
+++ scripts/storage.ts
@@ -1,19 +1,18 @@
-import { Contract } from "ethers"
+import { 
+  BigNumber,
+  Contract
+} from "ethers"
 import { ethers } from "hardhat"
 
-const coinName: string = "Storage"
-const coinAddr: string = "0xE3573540ab8A1C4c754Fd958Dc1db39BBE81b208"
+const contractName: string = "Storage"
+const contractAddress: string = "0xE3573540ab8A1C4c754Fd958Dc1db39BBE81b208"
 
 const main = async(): Promise<any> => {
-  const contract: Contract = await ethers.getContractAt(coinName, coinAddr)
-  const contractAddress: string = contract.address 
-  console.log(`Address: ${contractAddress}`)
-
-  let val: string = await contract.retrieve() 
-  console.log(`Val: ${val}`)
-
-  const store: string = await contract.store(507) 
-  console.log(store)
+  const contract: Contract = await ethers.getContractAt(contractName, contractAddress)
+  const num: BigNumber = await contract.retrieve()
+  console.log(`Number: ${num.toString()}`)
+  const tx = await contract.store(507)
+  console.log(tx)
 }
 
 main()
