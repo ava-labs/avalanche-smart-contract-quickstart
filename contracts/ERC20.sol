2f8c8f4c04703b95d013ddb9c6664cf081201ea6
Gabriel Cardona <gabriel@avalabs.org>
2021-05-25 16:59:46 -0700

    Add PR comments.

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier: mit
-pragma solidity ^0.8.0;
+pragma solidity >= 0.6.2 < 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ExampleERC20 is ERC20, AccessControl {
+contract ExampleERC20 is ERC20, Ownable {
   string private TOKEN_NAME = "Example ERC20 Token";
   string private TOKEN_SYMBOL = "XMPL";
 
@@ -12,18 +12,13 @@ contract ExampleERC20 is ERC20, AccessControl {
 
   constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) {
     _mint(msg.sender, TOTAL_SUPPLY);
-    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
   }
 
-  function decimals() public view virtual override returns (uint8) {
-   return 6;
-  }
-
-  function mint(address to, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
+  function mint(address to, uint256 amount) public onlyOwner() {
     _mint(to, amount);
   }
 
-  function burn(address from, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
+  function burn(address from, uint256 amount) public onlyOwner() {
     _burn(from, amount);
   }
 }
