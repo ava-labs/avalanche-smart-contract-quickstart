122fe055e5056c7eb21945183e96e117f0968a97
Gabriel Cardona <gabriel@avalabs.org>
2021-05-25 17:52:47 -0700

    Updates to ERC20 and ERC721 contracts.

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,24 +1,43 @@
 //SPDX-License-Identifier: mit
-pragma solidity >= 0.6.2 < 0.8.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract ExampleERC20 is ERC20, Ownable {
+contract ExampleERC20 is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable, ERC20Permit {
   string private TOKEN_NAME = "Example ERC20 Token";
   string private TOKEN_SYMBOL = "XMPL";
 
   uint private constant TOTAL_SUPPLY = 123456789;
 
-  constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) {
+  constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) ERC20Permit(TOKEN_NAME) {
     _mint(msg.sender, TOTAL_SUPPLY);
   }
 
-  function mint(address to, uint256 amount) public onlyOwner() {
+  function snapshot() public onlyOwner {
+    _snapshot();
+  }
+
+  function pause() public onlyOwner {
+    _pause();
+  }
+
+  function unpause() public onlyOwner {
+    _unpause();
+  }
+
+  function mint(address to, uint256 amount) public onlyOwner {
     _mint(to, amount);
   }
 
-  function burn(address from, uint256 amount) public onlyOwner() {
-    _burn(from, amount);
+  function _beforeTokenTransfer(address from, address to, uint256 amount)
+    internal
+    whenNotPaused
+    override(ERC20, ERC20Snapshot) {
+      super._beforeTokenTransfer(from, to, amount);
   }
 }
--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -1,24 +1,66 @@
 // contracts/ERC721.sol
 // spdx-license-identifier: mit
 
-pragma solidity >= 0.6.2;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract GameItem is ERC721 {
+contract ExampleERC721 is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burnable, ERC721Enumerable {
+  string private TOKEN_NAME = "GameItem";
+  string private TOKEN_SYMBOL = "ITEM";
   using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
 
-  constructor() ERC721("GameItem", "ITM") {}
+  Counters.Counter private _tokenIdCounter;
 
-  function awardItem(address player, string memory tokenURI) public returns (uint256) {
-    _tokenIds.increment();
+  constructor() ERC721(TOKEN_NAME, TOKEN_SYMBOL) {}
 
-    uint256 newItemId = _tokenIds.current();
-    _mint(player, newItemId);
-    // _setTokenURI(newItemId, tokenURI);
+  function safeMint(address to) public onlyOwner {
+    _safeMint(to, _tokenIdCounter.current());
+    _tokenIdCounter.increment();
+  }
 
-    return newItemId;
+  function pause() public onlyOwner {
+    _pause();
+  }
+
+  function unpause() public onlyOwner {
+    _unpause();
+  }
+
+  function _baseURI() internal pure override returns (string memory) {
+    return "https://example.com";
+  }
+
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    internal
+    whenNotPaused
+    override(ERC721, ERC721Enumerable) {
+      super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+  function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    super._burn(tokenId);
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory) {
+      return super.tokenURI(tokenId);
+  }
+
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, ERC721Enumerable)
+    returns (bool) {
+      return super.supportsInterface(interfaceId);
   }
 }
