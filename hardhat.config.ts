b5448ee8d1e5e6ea4990d32b969b1e54e1b356b9
paOol <paoolkim@gmail.com>
2021-07-18 19:45:12 -0700

    fix balance script not outputting anything

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,10 +26,12 @@ task("accounts", "Prints the list of accounts", async (args, hre): Promise<void>
 
 task("balances", "Prints the list of AVAX account balances", async (args, hre): Promise<void> => {
   const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
-  accounts.forEach(async (account: SignerWithAddress): Promise<void> => {
-    const balance: BigNumber = await hre.ethers.provider.getBalance(account.address)
-    console.log(`${account.address} has balance ${balance.toString()}`)
-  })
+  for(const account of accounts){
+    const balance: BigNumber = await hre.ethers.provider.getBalance(
+      account.address
+    );
+    console.log(`${account.address} has balance ${balance.toString()}`);
+  }
 })
 
 export default {
