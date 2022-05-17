9554c94625881bb0b2a1f20abee1432e3403c73e
Julian-dev28 <julian_martinez28@outlook.com>
2022-05-17 12:47:54 -0700

    update quick start for ANR

--- scripts/sendAvaxWalletSigner.ts
+++ scripts/sendAvaxWalletSigner.ts
@@ -9,6 +9,7 @@ const walletAddress: string = "0xDd1749831fbF70d88AB7bB07ef7CD9c53D054a57"
 const pk: string = "0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027"
 const protocol: string = "http"
 const host: string = "localhost"
+// Change to Node Port if using Avalanche Network Runner
 const port: number = 9650
 
 const main = async(): Promise<any> => {
